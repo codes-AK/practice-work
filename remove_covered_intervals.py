fcfe4b23fcde6c69863600a645b6c40d9c28b473
ak <aksharasajeevanm@gmail.com>
2026-07-06 23:07:07 +0800

    Implement removeCoveredIntervals method in Solution class

---
+++ remove_covered_intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        
+        count = 0
+        curr_max_end = 0
+        
+        for _, end in intervals:
+            if end > curr_max_end:
+                count += 1
+                curr_max_end = end
+                
+        return count
