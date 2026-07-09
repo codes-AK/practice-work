89c574c9bbe6a5a7efebf530e2c297ac7aded454
ak <aksharasajeevanm@gmail.com>
2026-07-09 23:11:05 +0800

    Add sorting to removeCoveredIntervals method

--- remove_covered_intervals.py
+++ remove_covered_intervals.py
@@ -1,6 +1,8 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: (x[0], -x[1]))
+
+
         
         count = 0
         curr_max_end = 0
